d7b2a8690bc48b0e3b6ec80b0eab50cc44b38e3e
Swarup Kharul <swarupkharulsk@gmail.com>
2021-03-15 11:17:57 +0530

    minor changes

--- stream/urls.py
+++ stream/urls.py
@@ -5,4 +5,5 @@ from stream import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('video_feed/', views.video_feed, name='video_feed'),
+    path('feature_feed/', views.feature_feed, name='feature_feed'),
     ]
--- stream/views.py
+++ stream/views.py
@@ -2,18 +2,31 @@ from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
 from stream.camera import VideoCamera
 from django.http import HttpResponse
+from django.template import loader, Context
+
+camera = VideoCamera()
 
 def index(request):
 	return render(request, 'streamapp/home.html')
 
-def gen(camera):
+
+def gen():
+	global camera
 	while True:
 		frame = camera.get_frame()
-		#feature = camera.render_features()
-		#print(feature)
 		yield (b'--frame\r\n'
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def video_feed(request):
-	return StreamingHttpResponse(gen(VideoCamera()),
+	return StreamingHttpResponse(gen(),
 					content_type='multipart/x-mixed-replace; boundary=frame')
+
+def features():
+	global camera
+	while True:
+		feature = camera.render_features()
+		yield (feature)
+
+def feature_feed(request):
+	return StreamingHttpResponse(features(),content_type='text/plain')
+
